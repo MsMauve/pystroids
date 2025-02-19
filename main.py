82ee78836c382f9f3cc6b67158d4abf82978c794
MsMauve <zoetropesilver@gmail.com>
2025-02-19 16:57:48 -0700

    refactor to establish object groups

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ def main():
     fps_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group() 
+    Player.containers = (updatable, drawable)
     player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player1.update(dt)
+        updatable.update(dt)
         screen.fill((0, 0, 0))
-        player1.draw(screen)
+        for drawn in drawable:
+            drawn.draw(screen)
         pygame.display.flip()
         dt = (fps_clock.tick(60)) / 1000
 
